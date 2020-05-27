8ca831143c74ec90d7a16b42ede184981d743eb5
Mark Dewey <markdewey@Mark-Dewey-MacBook-Pro.local>
2020-05-27 09:22:18 -0400

    things

---
+++ javascript/roman-numerals/roman-numerals.js
@@ -0,0 +1,20 @@
+//
+// This is only a SKELETON file for the 'Roman Numerals' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+
+const converter = {
+  'I': 1,
+  'V': 5,
+  'X': 10,
+  'L': 50,
+  "C": 100,
+  "D": 500,
+  'M': 1000,
+};
+
+
+export const toRoman = (number, current = "") => {
+
+};
